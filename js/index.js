500b84d9dd7331459638fa8c73bcb9794ed8d41a
ShadowedGold <77082877+ShadowedGold@users.noreply.github.com>
2023-06-04 00:35:05 +0800

    biomes, roads & areas of interest
    added mapDetails.js and a bunch of supporting functions to handle map details.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ var shapeType = 6;
 var angle = 2 * Math.PI / shapeType;
 var radius = 25;
 
+var mapHexOffset = [2,2];
+var ignoreFog = true;
+
 var canvas = document.createElement('canvas');
 
 canvas.id = "hexCanvas";
@@ -13,6 +16,8 @@ canvas.height = window.innerHeight - 12;
 document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
+var dimensions = drawGrid();
+
 function drawHex(x, y, num, colour) {
   ctx.beginPath();
   for (let i = 0; i < shapeType; i++) {
@@ -21,28 +26,137 @@ function drawHex(x, y, num, colour) {
     ctx.lineTo(xx, yy);
   }
   ctx.closePath();
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'dimgrey';
   ctx.stroke();
   ctx.fillStyle = colour;
   ctx.fill();
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = 'silver';
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-  ctx.fillText(num, x, y);
+  ctx.fillText("["+num+"]", x, y);
+
+  drawBiome(x, y, num);
+  drawRoad(x, y, num);
+  drawAoi(x, y, num);
 }
 
 function drawGrid() {
-  let cols = Math.ceil(canvas.width/(radius * 1.5));
+  let cols = Math.ceil(canvas.width/(radius * 1.5)) + 1;
   let rows = Math.ceil(canvas.height/(radius * Math.sin(angle) * 2)) + 1;
   let xOffset = canvas.width - ((cols-1) * radius * 1.5);
   let yOffset = canvas.height - ((rows-0.5) * radius * Math.sin(angle) * 2);
+  
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       drawHex((radius * i * 1.5) + (xOffset / 2),
               (radius * Math.sin(angle) * (i % 2)) + (radius * j * Math.sin(angle) * 2) + (yOffset / 2),
-              "["+i+","+j+"]",
-              (i == 0 || i == cols-1 || j == 0 || j == rows-1) ? "limegreen" : "greenyellow");     
+              [i,j],
+              (i == 0 || i == cols-1 || j == 0 || j == rows-1) ? "grey" : "darkgray");     
+    }
+  }
+
+  return [cols, rows];
+}
+
+function getSurroundingHexes(x, y) {
+  let hexSearchArr = [[-1, (x % 2) ? 0 : -1],
+                      [-1, (x % 2) ? 1 :  0],
+                      [ 0, -1],
+                      [ 0,  1],
+                      [ 1, (x % 2) ? 0 : -1],
+                      [ 1, (x % 2) ? 1 :  0]];
+  let hexFoundArr = [];
+
+  for (let i = 0; i < 6; i++) {
+    if (x + hexSearchArr[i][0] >= 0 &&
+        x + hexSearchArr[i][0] <= dimensions[0] - 1 &&
+        y + hexSearchArr[i][1] >= 0 &&
+        y + hexSearchArr[i][1] <= dimensions[1] - 1) {
+      hexFoundArr.push([x + hexSearchArr[i][0], y + hexSearchArr[i][1]]);
+    }
+  }
+
+  return hexFoundArr;
+}
+
+function drawBiome(x, y, num) {
+  let hexCoords = 'x' + (num[0] - mapHexOffset[0]) + 'y' + (num[1] - mapHexOffset[1]);
+  if (mapDetails.hasOwnProperty(hexCoords) &&
+     (mapDetails[hexCoords]['biomes']['known'] || ignoreFog)) {
+    let biomeNodes = mapDetails[hexCoords]['biomes']['value'];
+
+    for (let i = 0; i < biomeNodes.length; i++) {
+      ctx.beginPath();
+      ctx.lineTo(x, y);
+      let xx = x + radius * Math.cos(angle * (i-2));
+      let yy = y + radius * Math.sin(angle * (i-2));
+      ctx.lineTo(xx, yy);
+      xx = x + radius * Math.cos(angle * (i-1));
+      yy = y + radius * Math.sin(angle * (i-1));
+      ctx.lineTo(xx, yy);
+      ctx.closePath();
+      ctx.fillStyle = getBiomeColour(mapDetails[hexCoords]['biomes']['value'][i]);
+      ctx.fill();
     }
   }
 }
 
-drawGrid();
+function getBiomeColour(biome) {
+  switch (biome) {
+    case "plains":
+      return "greenyellow";
+    case "swamp":
+      return "darkseagreen";
+    case "forest":
+      return "limegreen";
+    case "mountain":
+      return "gainsboro";
+    case "water":
+      return "dodgerblue";
+    case "desert":
+      return "tan";
+    case "snow":
+      return "azure";
+  }
+}
+
+function drawRoad(x, y, num) {
+  let hexCoords = 'x' + (num[0] - mapHexOffset[0]) + 'y' + (num[1] - mapHexOffset[1]);
+  if (mapDetails.hasOwnProperty(hexCoords) &&
+     (mapDetails[hexCoords]['roads']['known'] || ignoreFog)) {
+    let roadNodes = mapDetails[hexCoords]['roads']['value'];
+
+    for (let i = 0; i < roadNodes.length; i++) {
+      ctx.beginPath();
+      ctx.lineTo(x, y);
+      let xx = x + radius * Math.cos(angle * (roadNodes[i]-1.5)) * Math.sqrt(3)/2;
+      let yy = y + radius * Math.sin(angle * (roadNodes[i]-1.5)) * Math.sqrt(3)/2;
+      ctx.lineTo(xx, yy);
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = 'sienna';
+      ctx.stroke();
+    }
+  }
+}
+
+function drawAoi(x, y, num) {
+  let hexCoords = 'x' + (num[0] - mapHexOffset[0]) + 'y' + (num[1] - mapHexOffset[1]);
+  if (mapDetails.hasOwnProperty(hexCoords) &&
+     (mapDetails[hexCoords]['aoi']['known'] || ignoreFog)) {
+    if (mapDetails[hexCoords]['aoi']['value'] != 0) {
+      ctx.beginPath();
+      ctx.arc(x, y, 1, 0, 2 * Math.PI, false)
+      ctx.closePath();
+      ctx.lineWidth = radius/5;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, 2 * Math.PI, false)
+      ctx.closePath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'red';
+      ctx.stroke();
+    }
+  }
+}
---
+++ js/mapDetails.js
@@ -0,0 +1,170 @@
+var mapDetails = {
+  x0y0: {
+    roads: {
+      value: [1,3],
+      known: true
+    },
+    biomes: {
+      value: [
+        "forest",
+        "plains",
+        "plains",
+        "plains",
+        "water",
+        "forest"
+      ],
+      known: true
+    },
+    aoi: {
+      value: 1,
+      known: true
+    }
+  },
+  x0y1: {
+    roads: {
+      value: [0,2],
+      known: true
+    },
+    biomes: {
+      value: [
+        "plains",
+        "plains",
+        "plains",
+        "water",
+        "water",
+        "water"
+      ],
+      known: true
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x0y2: {
+    roads: {
+      value: [],
+      known: true
+    },
+    biomes: {
+      value: [
+        "water",
+        "water",
+        "water",
+        "water",
+        "water",
+        "water"
+      ],
+      known: false
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x1y1: {
+    roads: {
+      value: [2,5],
+      known: true
+    },
+    biomes: {
+      value: [
+        "plains",
+        "plains",
+        "plains",
+        "swamp",
+        "water",
+        "plains"
+      ],
+      known: true
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x2y2: {
+    roads: {
+      value: [2,5],
+      known: true
+    },
+    biomes: {
+      value: [
+        "forest",
+        "plains",
+        "plains",
+        "plains",
+        "swamp",
+        "plains"
+      ],
+      known: false
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x3y2: {
+    roads: {
+      value: [2,5],
+      known: true
+    },
+    biomes: {
+      value: [
+        "plains",
+        "mountain",
+        "plains",
+        "plains",
+        "plains",
+        "plains"
+      ],
+      known: false
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x4y3: {
+    roads: {
+      value: [2,5],
+      known: true
+    },
+    biomes: {
+      value: [
+        "mountain",
+        "mountain",
+        "plains",
+        "plains",
+        "plains",
+        "plains"
+      ],
+      known: false
+    },
+    aoi: {
+      value: 0,
+      known: true
+    }
+  },
+  x10y5: {
+    roads: {
+      value: [1,3],
+      known: false
+    },
+    biomes: {
+      value: [
+        "plains",
+        "plains",
+        "plains",
+        "plains",
+        "plains",
+        "plains"
+      ],
+      known: false
+    },
+    aoi: {
+      value: 2,
+      known: true
+    }
+  }
+};
